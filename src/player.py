0b0716b748aabfd2b46a9a6c91e6299af7fdf54c
zoverlvx <zoverhulser@gmail.com>
2019-12-04 21:56:03 +0000

    Initial commit

--- src/player.py
+++ src/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location ## I feel like ABC needs to be used and the Room class needs to be passed here
