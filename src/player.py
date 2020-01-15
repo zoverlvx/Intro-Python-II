e7f8c0007a04b5ab9518be2b8b227bae48ff90e5
zoverlvx <zoverhulser@gmail.com>
2020-01-15 21:56:11 +0000

    Simplifies class

--- src/player.py
+++ src/player.py
@@ -5,5 +5,5 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room 
-    def states_location(self):
-        print(F"{self.name} is in the {self.current_room.name} which is {self.current_room.description}")
+    def __str__(self):
+        return "{self.name} is in the {self.current_room.name} \n{self.current_room.description}".format(self=self)
