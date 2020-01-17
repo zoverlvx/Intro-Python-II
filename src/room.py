5c50157a41cea313e8d8220fb7dfc6584a54ff72
zoverlvx <zoverhulser@gmail.com>
2020-01-17 01:00:32 +0000

    Displays items in room

--- src/room.py
+++ src/room.py
@@ -9,3 +9,12 @@ class Room:
         self.items = items
     def show_items(self):
         return F"{self.name} has {len(self.items)} items in it."
+    def display_item_choices(self):
+        if len(self.items) >= 1:
+            i = 1
+            options = ""
+            for item in self.items:
+                options += F"{str(i)}. {item} "
+                i += 1
+            return F"Here are the items to choose from: {options}\nType n for none."
+        else: return ""
